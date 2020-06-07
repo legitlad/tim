3ff35a0c5453d2af82bae269162a432b0e39a88e
legitlad <wassuplad@gmail.com>
2020-06-07 11:02:35 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+
+class MyGrid(Widget):
+    firstname = ObjectProperty(None)
+    lastname = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("First Name:", self.firstname.text , "Last Name:", self.lastname.text, "Email:", self.email.text)
+        self.firstname.text = ""
+        self.lastname.text = ""
+        self.email.text = ""
+
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
